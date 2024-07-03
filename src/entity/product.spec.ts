9269dd92045df70fe4106507b4d654f236ebf88f
Santiago Negreira <sansfleitas@gmail.com>
2024-07-03 11:01:32 -0300

    Create Product Aggregate
    * create the Product class
    * create unit test
    * set ECMA2020 as default

---
+++ src/entity/product.spec.ts
@@ -0,0 +1,33 @@
+import Product from "./product";
+
+describe("Product unit test suite", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      new Product("", "Product 1", 100);
+    }).toThrow("Id is required");
+  });
+
+  it("should throw error when name is empty", () => {
+    expect(() => {
+      new Product("123", "", 100);
+    }).toThrow("Name is required");
+  });
+
+  it("should throw error when price is less than 0", () => {
+    expect(() => {
+      new Product("123", "Name", -1);
+    }).toThrow("Price must be greater than 0");
+  });
+
+  it("should change name", () => {
+    const product = new Product("123", "Name", 100);
+    product.changeName("New Name");
+    expect(product.name).toBe("New Name");
+  });
+
+  it("should change price", () => {
+    const product = new Product("123", "Name", 100);
+    product.changePrice(200);
+    expect(product.price).toBe(200);
+  });
+});
