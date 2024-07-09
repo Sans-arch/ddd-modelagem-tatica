0d6f833d0fac404f4cffb5ad772f07f29a3445c9
Santiago Fleitas <sansfleitas@gmail.com>
2024-07-09 12:59:46 -0300

    Add customer rewards unit test

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -46,4 +46,15 @@ describe("Customer test suite", () => {
 
     expect(customer.isActive()).toBe(false);
   });
+
+  it("should add reward points", () => {
+    const customer = new Customer("1", "Customer 1");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
+  });
 });
