f43ab213d391f4ec282541c1e730f093dbc4078e
Santiago Fleitas <sansfleitas@gmail.com>
2024-07-11 17:34:39 -0300

    Create EventDispatcher, create ProductCreated Event and one handler for it

---
+++ src/event/product/product-created.event.ts
@@ -0,0 +1,11 @@
+import EventInterface from "../@shared/event.interface";
+
+export default class ProductCreatedEvent implements EventInterface {
+  dataTimeOccured: Date;
+  eventData: any;
+
+  constructor(eventData: any) {
+    this.dataTimeOccured = new Date();
+    this.eventData = eventData;
+  }
+}
