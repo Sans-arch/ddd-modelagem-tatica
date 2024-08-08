e94d25b4e9cc0768f3c22fb86041d16e6eb4bb54
Santiago Negreira <sansfleitas@gmail.com>
2024-08-08 15:26:01 -0300

    Create more unit tests for CreateCustomer Usecase

--- src/usecase/customer/create/create.customer.unit.spec.ts
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -37,4 +37,22 @@ describe("Unit test create customer use case", () => {
       },
     });
   });
+
+  it("should thrown an error when name is missing", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+    input.name = "";
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow("Name is required");
+  });
+
+  it("should thrown an error when street is missing", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+    input.address.street = "";
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow("Street is required");
+  });
 });
