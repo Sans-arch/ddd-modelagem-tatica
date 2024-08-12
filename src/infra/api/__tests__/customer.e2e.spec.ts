37484169c04c5ae4275897010441cd01116df4ac
Santiago Negreira <sansfleitas@gmail.com>
2024-08-12 14:46:04 -0300

    Create more e2e test

--- src/infra/api/__tests__/customer.e2e.spec.ts
+++ src/infra/api/__tests__/customer.e2e.spec.ts
@@ -30,4 +30,12 @@ describe("E2E test for customer", () => {
     expect(response.body.address.number).toBe(123);
     expect(response.body.address.zip).toBe("12345");
   });
+
+  it("should not create a customer", async () => {
+    const response = await request(app).post("/customer").send({
+      name: "John",
+    });
+
+    expect(response.status).toBe(500);
+  });
 });
