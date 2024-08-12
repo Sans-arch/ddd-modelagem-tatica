26829f905349bc89b388694207b7ee7288f1ad9b
Santiago Negreira <sansfleitas@gmail.com>
2024-08-12 12:51:23 -0300

    Create API adapter (using Express)
    * Install Express to be our REST implementation
    * Create a REST API adapter to plug in our app

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Projeto prático sobre a utilização de táticas do DDD e Clean Architecture
 
 Neste projeto são usadas várias táticas do DDD e além disso também é aplicada a Clean Architecture.
+
+Conceitos abordados e aplicados:
+
+- Criação de testes unitários (usando TDD)
+- Criação de Domain Entities, Domain Services e Domain Events
+- Criação de Usecases
+- Aplicação de alguns design patterns como o padrão Repository, Factory e DTO.
+- Criação de API Rest
+- Utilização de variáveis de ambiente
---
+++ src/infra/api/express.ts
@@ -0,0 +1,19 @@
+import express, { Express } from "express";
+import { Sequelize } from "sequelize-typescript";
+import CustomerModel from "../customer/repository/sequelize/customer.model";
+
+export const app: Express = express();
+app.use(express.json());
+
+export let sequelize: Sequelize;
+
+async function setupDb() {
+  sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: ":memory:",
+    logging: false,
+  });
+  sequelize.addModels([CustomerModel]);
+  await sequelize.sync();
+}
+setupDb();
