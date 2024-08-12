116a3af2a43b18ca5791c5d7a81fafa292abdca8
Santiago Negreira <sansfleitas@gmail.com>
2024-08-12 14:57:23 -0300

    Create ListCustomer route and e2e test

--- src/infra/api/__tests__/customer.e2e.spec.ts
+++ src/infra/api/__tests__/customer.e2e.spec.ts
@@ -38,4 +38,42 @@ describe("E2E test for customer", () => {
 
     expect(response.status).toBe(500);
   });
+
+  it("should list all customers", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const response2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jane",
+        address: {
+          street: "Street 2",
+          city: "City 2",
+          number: 1234,
+          zip: "12344",
+        },
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/customer").send();
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.customers.length).toBe(2);
+    const customer = listResponse.body.customers[0];
+    expect(customer.name).toBe("John");
+    expect(customer.address.street).toBe("Street");
+    const customer2 = listResponse.body.customers[1];
+    expect(customer2.name).toBe("Jane");
+    expect(customer2.address.street).toBe("Street 2");
+  });
 });
--- src/infra/api/routes/customer.route.ts
+++ src/infra/api/routes/customer.route.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
+import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
 
 export const customerRoute = express.Router();
 
 customerRoute.post("/", async (req: Request, res: Response) => {
   const usecase = new CreateCustomerUseCase(new CustomerRepository());
-
   try {
     const customerDto = {
       name: req.body.name,
@@ -19,6 +19,16 @@ customerRoute.post("/", async (req: Request, res: Response) => {
     };
 
     const output = await usecase.execute(customerDto);
+    return res.json(output);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+  const usecase = new ListCustomerUseCase(new CustomerRepository());
+  try {
+    const output = await usecase.execute({});
     return res.json(output);
   } catch (err) {
     return res.status(500).send(err);
